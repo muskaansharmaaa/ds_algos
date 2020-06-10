0ceb1db4adc4a684cf4d26162b86fe9ff997d3d8
muskaansharmaaa <54302217+muskaansharmaaa@users.noreply.github.com>
2020-06-10 22:35:32 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,1 +1,59 @@
 
+import java.util.*;
+
+
+class BubbleSort{
+
+
+static void bubblesort(int[] arr,int size)
+{
+     
+     for(int i=0;i<size;i++)
+     {
+         for(int j=0;j<size-1;j++)
+         {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+
+         }
+     }
+
+    print(arr,size); 
+
+}
+
+static void print(int[] array,int size)
+{
+    System.out.println("After Sort:");
+   for(int i=0;i<size;i++)
+   {
+       System.out.print(array[i]);
+   }
+}
+
+public static void main(String[] args)
+{
+     Scanner sc=new Scanner(System.in);
+
+     System.out.println("Enter the size of the array:");
+     int size=sc.nextInt();
+     int[] arr=new int[size];
+
+
+     System.out.println("Enter the elements of the array:");
+      System.out.println("Before Sort:");
+     for(int i=0;i<size;i++)
+     {
+         arr[i]=sc.nextInt();
+     }
+
+    bubblesort(arr,size);
+    
+
+
+}
+}
